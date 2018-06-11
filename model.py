95fc993eeb24d559f0ec9a2f1553c9a37b4c1e63
tboquet <thomas.boquet@hec.ca>
2018-06-11 17:15:03 -0400

    + clean

--- model.py
+++ model.py
@@ -4,10 +4,5 @@ import numpy as np
 class Model:
     """Dummy model"""
     def generate(self, inputs):
-<<<<<<< HEAD
         shape = (1000, 128, 128, 3)
-        return np.random.uniform(size=np.prod(shape)).reshape(shape)
-=======
-        shape = (32, 128, 128, 3)
         return np.random.uniform(size=np.prod(shape)).reshape(shape).astype(np.uint8)
->>>>>>> bc48b034620d1cee4887d8982fec8b71018b3f93
