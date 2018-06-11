5868615a7774cc91cfe6c93149a14a1721a3f9a9
tboquet <thomas.boquet@hec.ca>
2018-06-11 17:13:55 -0400

    32 -> 1000 images

--- model.py
+++ model.py
@@ -4,5 +4,5 @@ import numpy as np
 class Model:
     """Dummy model"""
     def generate(self, inputs):
-        shape = (32, 128, 128, 3)
+        shape = (1000, 128, 128, 3)
         return np.random.uniform(size=np.prod(shape)).reshape(shape)
